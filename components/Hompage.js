d3238a94350a17b80cd85ce497b9cd9a1a6a87cc
Kalum <kshorty@live.co.uk>
2019-02-14 15:10:46 +0000

    added view legends button on homepage

--- components/Hompage.js
+++ components/Hompage.js
@@ -4,7 +4,10 @@ import {
         Text, 
         TouchableHighlight, 
         FlatList, 
-        StyleSheet
+        StyleSheet,
+        ImageBackground,
+        Dimensions,
+        ScrollView
         } from 'react-native';
 
 class HomePage extends Component {
@@ -14,12 +17,47 @@ class HomePage extends Component {
     render() {
         return(
             <>
-            <Text>
-                homepage
-            </Text>
+            <View style={styles.container}>
+                <ScrollView>
+                    <TouchableHighlight
+                    // onPress={handlePress}
+                    >
+                        <ImageBackground
+                        style={styles.legendsImage}
+                        source={require('../assets/apex-embed-legends-lineup.png')}>
+                            <Text style={styles.legendsText}>View Legends</Text>
+                        </ImageBackground>
+                    </TouchableHighlight>
+                </ScrollView>
+            </View>
             </>
         );
     }
 }
 
 export default HomePage;
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    legendsImage: {
+        flex:1,
+        justifyContent: 'center',
+        width: WIDTH*1,
+        height: HEIGHT*0.4,
+    
+    },
+    legendsText: {
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: '#DA292A',
+        margin: WIDTH*0.25,
+        fontSize:20,
+        padding: 4,
+        borderRadius: 10,
+    }
+})
