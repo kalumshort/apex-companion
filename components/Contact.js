a951c8c1300a46a6e7f0f5e2037d9ea5b9682713
Kalum <kshorty@live.co.uk>
2019-02-27 14:17:04 +0000

    created a contact page with social links

--- components/Contact.js
+++ components/Contact.js
@@ -7,8 +7,15 @@ import {
         StyleSheet,
         ImageBackground,
         Dimensions,
-        ScrollView
+        ScrollView,
+        Linking
         } from 'react-native';
+import { 
+    Ionicons,
+    MaterialCommunityIcons,
+    FontAwesome,
+
+    } from '@expo/vector-icons';
 
 class Contact extends Component {
     constructor(props){
@@ -17,8 +24,36 @@ class Contact extends Component {
     render() {
         return(
             <>
-            <View>
-                
+            <View style={styles.container}>
+                <View style={styles.contact}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>Contact Me!</Text>
+                    </View>
+                    <View style={styles.inqureContainer}>
+                        <Text style={styles.inquireText}>To inquire about my work please E-Mail: </Text>
+                        <Text style={styles.inquireEmai}>kalumdarcyshort@outlook.com</Text>
+                    </View>
+                    <View style={styles.helpContainer}>
+                        <Text style={styles.helpText}>To send me surgestions about content you want adding to this app please E-Mial </Text>
+                        <Text style={styles.helpEmail}>kalumapexapp.outlook.com</Text>
+                    </View>
+                </View>
+                <View style={styles.follow}>
+                    <View style={styles.socials}>
+                        <FontAwesome
+                            name="twitter"
+                            size={50}
+                            onPress={() => Linking.openURL('https://twitter.com/KalumShort_')}
+                            style={styles.icons}
+                        />
+                        <FontAwesome
+                            name="instagram"
+                            size={50}
+                            onPress={() => Linking.openURL('https://www.instagram.com/kalumshort/')}
+                            style={styles.icons}
+                        />
+                    </View>
+                </View>
             </View>
             </>
         );
@@ -34,22 +69,75 @@ const HEIGHT = Dimensions.get('window').height;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: 'grey',
+        alignItems: 'center',
     },
-    itemImage: {
-        flex:1,
-        justifyContent: 'center',
+    contact: {
+        flex: 1,
+    },
+    titleContainer: {
         width: WIDTH*1,
-        height: HEIGHT*0.33,
-    
+        borderBottomWidth: 1,
+        borderColor: 'white',
     },
-    itemText: {
-        textAlign: 'center',
+    title:{
         color: 'white',
-        backgroundColor: '#DA292A',
-        margin: WIDTH*0.25,
-        fontSize:20,
-        padding: 4,
-        borderRadius: 10,
-    }
+        fontSize: 25,
+        marginTop: 10,
+        textAlign: 'center',
+        
+    },
+    inqureContainer: {
+        alignItems: 'center',
+        width: WIDTH*1,
+        borderBottomWidth: 1,
+        borderColor: 'white',
+        marginTop: 30,
+     
+    },
+    inquireText: {
+        color: 'white',
+        fontSize: 19,
+        fontStyle: 'italic',
+        margin: 7,
+        textAlign: 'center',
+        fontWeight: '100',
+    },
+    inquireEmai: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '900',
+    },
+    helpContainer: {
+        marginTop: 30,
+        alignItems: 'center',
+        width: WIDTH*1,
+        borderBottomWidth: 1,
+        borderColor: 'white',
+    },
+    helpText: {
+        color: 'white',
+        fontSize: 19,
+        fontStyle: 'italic',
+        margin: 7,
+        textAlign: 'center',
+        fontWeight: '100',
+    },
+    helpEmail: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '900',
+    },
+    follow: {
+
+    },
+    socials: { 
+        flexDirection: 'row',
+
+    },
+    icons: {
+        color: 'white',
+        margin: 30,
+    },
+   
 })
