a6ade3eec75bb556a8409987840d327c22fa5355
Kalum <kshorty@live.co.uk>
2019-03-04 10:11:16 +0000

    added all the healing data

--- components/Health.js
+++ components/Health.js
@@ -26,8 +26,37 @@ class HealthPage extends Component {
             <>
             <View style={styles.container}>
                 <ScrollView>
-                    <View>
-                        <Text>Health</Text>
+                    <Text style={styles.healingHeader}>Healing Items</Text>
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Syringe</Text>
+                        <Text style={styles.healthBenefit}>Heals 25 HP</Text>
+                        <Text style={styles.healthTime}>(5s Use Time)</Text>
+                    </View> 
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Med Kit</Text>
+                        <Text style={styles.healthBenefit}>Heals 100 HP</Text>
+                        <Text style={styles.healthTime}>(8s Use Time)</Text>
+                    </View>
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Phoenix Kit</Text>
+                        <Text style={styles.healthBenefit}>Heals 100 HP</Text>
+                        <Text style={styles.healthBenefit}>Restores 100 Shield</Text>
+                        <Text style={styles.healthTime}>(10s Use Time)</Text>
+                    </View>
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Shield Cell</Text>
+                        <Text style={styles.healthBenefit}>Restores 25 Shield</Text>
+                        <Text style={styles.healthTime}>(3s Use Time)</Text>
+                    </View>
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Shield Battery</Text>
+                        <Text style={styles.healthBenefit}>Restores 100 Shield</Text>
+                        <Text style={styles.healthTime}>(5s Use Time)</Text>
+                    </View>
+                    <View style={styles.healthItem}>
+                        <Text style={styles.healthName}>Ultimate Accelerant</Text>
+                        <Text style={styles.healthBenefit}>Restores 20% Ultimate</Text>
+                        <Text style={styles.healthTime}>(7s Use Time)</Text>
                     </View>
                 </ScrollView>
             </View>
@@ -47,6 +76,29 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'grey',
         alignItems: 'center'
+    },
+    healingHeader: {
+        color: 'white',
+        fontSize: 25,
+    },
+    healthItem: {
+        alignItems: 'center',
+        flex: 1,
+        width: WIDTH*0.8,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: 'white',
+        margin: 10,
+        padding: 10,
+    },
+    healthName: {
+        color: 'white',
+    },
+    healthBenefit: {   
+        color: 'white',
+    },
+    healthTime: {
+        color: 'white',
     },
 
 })
